ac091be0bdad3373684021fbc3e953277a31a533
ptran12 <phitran.722@gmail.com>
2020-10-01 22:38:42 -0500

    fetching data from api test

--- covid-19-dash/src/App.js
+++ covid-19-dash/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css';
+import { fetchData } from './api';
+import { render } from '@testing-library/react';
 
 
-function App() {
-  return (
-    <>
-    <Cards/>
-    <CountryPicker/>
-    <Chart/>
-    </>
-  );
+class App extends React.Component {
+  
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+      <Cards/>
+      <CountryPicker/>
+      <Chart/>
+      </div>
+    );
+  }
 }
 
 export default App;
