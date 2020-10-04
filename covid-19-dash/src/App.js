4bd19fc57a9cb5a981eeb924c0972563e7774dbe
ptran12 <phitran.722@gmail.com>
2020-10-03 19:02:40 -0500

    install material-ui & created card

--- covid-19-dash/src/App.js
+++ covid-19-dash/src/App.js
@@ -3,20 +3,27 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css';
 import { fetchData } from './api';
-import { render } from '@testing-library/react';
 
 
 class App extends React.Component {
+
+  state = {
+    data: {},
+  }
   
   async componentDidMount() {
-    const data = await fetchData();
-    console.log(data);
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData })
   }
 
   render() {
+
+    const { data } = this.state
+
     return (
       <div className={styles.container}>
-      <Cards/>
+      <Cards data={data}/>
       <CountryPicker/>
       <Chart/>
       </div>
