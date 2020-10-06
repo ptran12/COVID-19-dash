a0ecbadb7b33f8dabbfc53672b0dfc72b58004da
ptran12 <phitran.722@gmail.com>
2020-10-06 14:07:05 -0500

    added extra cards and gen api #s to card

--- covid-19-dash/src/components/Cards/Cards.jsx
+++ covid-19-dash/src/components/Cards/Cards.jsx
@@ -3,17 +3,38 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
-    console.log(props); 
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+    if(!confirmed) {
+        return "Loading...."
+    }
+    console.log(confirmed);
     return(
        <div className = {styles.container}>
            <Grid container spacing={3} justify="center">
                 <Grid item component={Card}> 
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">data filler</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography>
                         <Typography color="textSecondary">date filler</Typography>
                         <Typography variant="body2"># of active cases</Typography>
+                    </CardContent>
+                </Grid>
+                {/* Recovered */}
+                <Grid item component={Card}> 
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">data filler</Typography>
+                        <Typography color="textSecondary">date filler</Typography>
+                        <Typography variant="body2"># of recoveries</Typography>
+                    </CardContent>
+                </Grid>
+                {/* Deaths */}
+                <Grid item component={Card}> 
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">data filler</Typography>
+                        <Typography color="textSecondary">date filler</Typography>
+                        <Typography variant="body2"># of deaths</Typography>
                     </CardContent>
                 </Grid>
            </Grid>
